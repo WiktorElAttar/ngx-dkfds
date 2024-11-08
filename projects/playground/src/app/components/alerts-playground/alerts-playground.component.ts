60a2693ac37ac5ea8e360fd04d139db185ae3b65
WiktorElAttar <wiktor.elattar@gmail.com>
2024-11-08 16:56:43 +0100

    Update alerts-playgrounbd.component to use fds-alert

--- projects/playground/src/app/components/alerts-playground/alerts-playground.component.ts
+++ projects/playground/src/app/components/alerts-playground/alerts-playground.component.ts
@@ -1,8 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ErrorAlertComponent } from '../../../../../ngx-dkfds/src/lib/alerts/error-alert/error-alert.component';
-import { InfoAlertComponent } from '../../../../../ngx-dkfds/src/lib/alerts/info-alert/info-alert.component';
-import { SuccessAlertComponent } from '../../../../../ngx-dkfds/src/lib/alerts/success-alert/success-alert.component';
-import { WarningAlertComponent } from '../../../../../ngx-dkfds/src/lib/alerts/warning-alert/warning-alert.component';
+import { AlertComponent } from '../../../../../ngx-dkfds/src/lib/alerts/alert/alert.component';
 
 @Component({
   selector: 'app-alerts-playground',
@@ -10,6 +7,7 @@ import { WarningAlertComponent } from '../../../../../ngx-dkfds/src/lib/alerts/w
   templateUrl: './alerts-playground.component.html',
   styleUrl: './alerts-playground.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [InfoAlertComponent, SuccessAlertComponent, WarningAlertComponent, ErrorAlertComponent],
+  imports: [AlertComponent],
 })
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class AlertsPlaygroundComponent {}
