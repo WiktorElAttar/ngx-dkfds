8c6f9a8fd43a0ec9b35c94f303d1b0258e3d0456
WiktorElAttar <wiktor.elattar@gmail.com>
2024-11-08 17:01:43 +0100

    @typescript-eslint/no-extraneous-class in card-playground.component.ts

--- projects/playground/src/app/components/card-playground/card-playground.component.ts
+++ projects/playground/src/app/components/card-playground/card-playground.component.ts
@@ -10,4 +10,5 @@ import { CardComponent } from '../../../../../ngx-dkfds/src/lib/card/card.compon
   styleUrl: './card-playground.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class CardPlaygroundComponent {}
