8af3ddecf30fca69fcbd5ebd3f057dcd3a1ede38
WiktorElAttar <wiktor.elattar@gmail.com>
2024-11-09 14:34:22 +0100

    Update import

--- projects/playground/src/app/app.routes.ts
+++ projects/playground/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { AlertsPlaygroundComponent } from './components/alerts-playground/alerts-playground.component';
-import { CardPlaygroundComponent } from './components/card-playground/card-playground.component';
+import { AlertsPlaygroundComponent } from './playgrounds/alerts-playground/alerts-playground.component';
+import { CardPlaygroundComponent } from './playgrounds/card-playground/card-playground.component';
 
 export const routes: Routes = [
   {
