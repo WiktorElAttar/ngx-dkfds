ac01353ce47328e748b504541a24e176964ac02e
WiktorElAttar <wiktor.elattar@gmail.com>
2024-11-09 16:55:53 +0100

    Add badge component

---
+++ projects/ngx-dkfds/src/lib/badge/badge.component.ts
@@ -0,0 +1,17 @@
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { FdsBadgeType } from './badge-type.model';
+
+@Component({
+  selector: 'fds-badge',
+  standalone: true,
+  templateUrl: './badge.component.html',
+  styleUrl: './badge.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgClass],
+})
+export class BadgeComponent {
+  public text = input.required<string>();
+  public type = input.required<FdsBadgeType>();
+  public size = input.required<'small' | 'large'>();
+}
